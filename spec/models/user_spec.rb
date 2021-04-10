777186a46a4da6a85e8392a97c38f4b045417393
Vanessa <vannexxaa@gmail.com>
2021-04-10 14:49:12 +0000

    Create user unittests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe '#validations' do
+    let(:user) {User.new()}
+
+    it 'without email for the user' do
+      user.email = nil
+      expect(user).not_to be_valid
+    end
+
+    it 'with invalid email for the user' do
+      user.email = "name@gmail"
+      expect(user).not_to be_valid
+    end
+
+    it 'when another user has a duplicated email' do
+      user2 = User.new
+      user2.email = "email@gmail.com"
+      user.email = "email@gmail.com"
+      expect(user).not_to be_valid
+    end
+
+    it 'without password for the user' do
+      user.password = nil
+      expect(user).not_to be_valid
+    end
+
+    it 'without password confirmation for the user' do
+      user.password_confirmation = nil
+      expect(user).not_to be_valid
+    end
+
+    it 'with wrong password confirmation for the user' do
+      user.password = "pass00"
+      user.password_confirmation="pass01"
+      expect(user).not_to be_valid
+    end
+
+  end
 end
