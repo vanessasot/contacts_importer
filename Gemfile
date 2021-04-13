2bdad4f9bfbabc76d91456dfe997aebd9044f400
Vanessa <vannexxaa@gmail.com>
2021-04-13 20:57:47 +0000

    Add credit card and credit card franchise validation

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ ruby '2.7.0'
 gem 'rails', '~> 6.1.3', '>= 6.1.3.1'
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 1.1'
+# Use for validate credit card
+gem 'credit_card_validations'
 # Use Puma as the app server
 gem 'puma', '~> 5.0'
 # Use SCSS for stylesheets
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -7,6 +7,7 @@ class Contact < ApplicationRecord
     validates :telephone,
               format: { with: /\A(\(\+\d{2}\)[\s]\d{3}[\s]\d{3}[\s]\d{2}[\s]\d{2})|(\(\+\d{2}\)[\s]\d{3}[-]\d{3}[-]\d{2}[-]\d{2})\z/,
               message: "(+00) 000 000 00 00 and (+00) 000-000-00-00 are the only telephone formats permitted" }
+    validates :credit_card_number, credit_card_number: true
 
     def date_valid
         Date.iso8601(birthday)
@@ -19,7 +20,7 @@ class Contact < ApplicationRecord
                                          email: contact_hash['email'], address: contact_hash['address'],
                                          birthday: contact_hash['birthday'],
                                          credit_card_number: contact_hash['credit_card_number'],
-                                         credit_card_franchise: contact_hash['credit_card_franchise'])
+                                         credit_card_franchise: contact_hash['credit_card_number'].credit_card_brand_name)
             contact.update(contact_hash)
         end
     end
