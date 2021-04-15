2a8050aa6ad096ceef7f7dfe5afb0571d3e7f9ed
Vanessa <vannexxaa@gmail.com>
2021-04-15 17:12:46 +0000

    Update birthday method

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -1,5 +1,11 @@
 module ContactsHelper
     def format_birthday(date)
-        Date.iso8601(date).strftime('%Y %B %e')
+      format_date = ''
+      begin
+        format_date = Date.iso8601(date).strftime('%Y %B %e')
+      rescue StandardError
+        format_date = date
+      end
+      format_date
     end
 end
